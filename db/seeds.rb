814df3ac03dc8ecb9fae1d1db70ec7f8b30ec8ed
dextrizzle <alfonzo_01@hotmail.com>
2017-04-03 14:12:14 -0700

    add seed for categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+6.times do
+  Category.create name: Faker::Hacker.noun
+end
+
+puts 'created 6 products'
+
 1000.times do
-  # Product.create title: Faker::Hacker.say_something_smart,
-  #                 description: Faker::Hacker.say_something_smart,
-  #                 price: rand(1000)
-  # User.create first_name: Faker::Name.first_name,
-  #             last_name: Faker::Name.last_name,
-  #             email: Faker::Internet.email
+  category = Category.all.sample
+
+  Product.create title: Faker::Hacker.say_something_smart,
+                  description: Faker::Hacker.say_something_smart,
+                  price: rand(1000),
+                  category_id: category.id
+  User.create first_name: Faker::Name.first_name,
+              last_name: Faker::Name.last_name,
+              email: Faker::Internet.email
 end
 
 puts 'Created 1000 products'
